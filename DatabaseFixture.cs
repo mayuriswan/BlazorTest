96c6ad2a26ef7e9ac7951e940e82f2e58e199df9
Ayoub El haoussi <aelhaoussi@sqli.com>
2024-07-03 20:25:10 +0100

    first

---
+++ DatabaseFixture.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Blazor.Services.Tests;
+
+public class DatabaseFixture : IDisposable
+{
+    public IDbContextFactory<AppDbContext> DbContextFactory { get; }
+
+    public DatabaseFixture()
+    {
+        var services = new ServiceCollection();
+        services.AddDbContextFactory<AppDbContext>(options =>
+            options.UseInMemoryDatabase("InMemoryDb"));
+
+        var serviceProvider = services.BuildServiceProvider();
+        DbContextFactory = serviceProvider.GetRequiredService<IDbContextFactory<AppDbContext>>();
+    }
+
+    public void Dispose()
+    {
+        // Clean up the in-memory database if needed
+    }
+}
+
+[CollectionDefinition("Database collection")]
+public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
+{
+}
